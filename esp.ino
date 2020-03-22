ed0f9929bf952bf5ebce49622c7c9bffe59a2634
daint23 <rahmadamo05@gmail.com>
2020-03-22 13:42:21 +0800

    S

--- esp.ino
+++ esp.ino
@@ -128,7 +128,6 @@ void setup() {
   bool status;
 
   // default settings
-  // (you can also pass in a Wire library object like &Wire2)
   status = bme.begin(0x76);  
   if (!status) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
